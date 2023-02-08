ae906b0bf5b3a9798571163c6a42697cdfac01b6
김혁수 <hyuksu0308@naver.com>
2023-02-08 15:57:53 +0900

    [feat] 알림 모달 html 구현

--- server/apps/matchingMatch/urls.py
+++ server/apps/matchingMatch/urls.py
@@ -3,11 +3,11 @@ from . import views
 app_name = "matchingMatch"
 
 urlpatterns = [
-    path("match_register",views.match_register, name="match_register"),
+    path("match_register", views.match_register, name="match_register"),
     path("", views.main, name="main"),
-    path("endOfGame/", views.endOfGame, name="endOfGame"),
     path('login/', views.login_page, name="login"),
     path('register/', views.register_page, name="register"),
     path('logout/', views.logout_user, name="logout"),
     path('account/', views.account_page, name="account"),
+    path('rate/', views.account_page, name="rate"),
 ]
--- server/apps/matchingMatch/views.py
+++ server/apps/matchingMatch/views.py
@@ -11,69 +11,61 @@ import datetime
 # Create your views here.
 
 
+def match_detail(request, pk):  # pk = 매치 아이디
 
+    user = request.user
 
+    match = get_object_or_404(MatchInfo, pk=pk)
 
-def match_detail(request, pk): # pk = 매치 아이디
-
-  user = request.user
-
-  match = get_object_or_404(MatchInfo, pk = pk)
-
-  context = {"user" : user, "match" : match}
-
-  return render(request, "html", context=context)
-
-
-
-def team_detail(request, pk): # pk = 팀 아이디
-
-  user = request.user
-
-  match = get_object_or_404(Team, pk = pk)
-
-  context = {"user" : user, "match" : match}
-
-  return render(request, "html", context=context)
-
-
-
-
-def team_update(request, pk):
-  match = get_object_or_404(Team, pk = pk)
-  if request.method == "POST":
-    match_form = CustomUserCreateForm(request.POST)
-    if match_form.is_valid():
-      match_form.save()
-      return redirect("/")
-    else:
-      return render()
-  else:
-    match_form = CustomUserCreateForm(instance=match)
-    context = {"match_form" : match_form}
+    context = {"user": user, "match": match}
 
     return render(request, "html", context=context)
 
 
+def team_detail(request, pk):  # pk = 팀 아이디
 
-def my_page(request, pk): # pk = 유저 아이디
-  #아직 어떤 기능을 넣을지 미정
-  pass
+    user = request.user
 
+    match = get_object_or_404(Team, pk=pk)
+
+    context = {"user": user, "match": match}
+
+    return render(request, "html", context=context)
+
+
+def team_update(request, pk):
+    match = get_object_or_404(Team, pk=pk)
+    if request.method == "POST":
+        match_form = CustomUserCreateForm(request.POST)
+        if match_form.is_valid():
+            match_form.save()
+            return redirect("/")
+        else:
+            return render()
+    else:
+        match_form = CustomUserCreateForm(instance=match)
+        context = {"match_form": match_form}
+
+        return render(request, "html", context=context)
+
+
+def my_page(request, pk):  # pk = 유저 아이디
+    # 아직 어떤 기능을 넣을지 미정
+    pass
 
 
 def match_register(request):
-  
-  if request.method == "POST":
-    match_form = MatchRegisterForm(request.POST)
-    if match_form.is_valid():
-      match_form.save()
-      return redirect("/")
-  
-  match_form = MatchRegisterForm()
-  context = {"match_form" : match_form}
 
-  return render(request, "matchingMatch/match_register.html", context=context)
+    if request.method == "POST":
+        match_form = MatchRegisterForm(request.POST)
+        if match_form.is_valid():
+            match_form.save()
+            return redirect("/")
+
+    match_form = MatchRegisterForm()
+    context = {"match_form": match_form}
+
+    return render(request, "matchingMatch/match_register.html", context=context)
 
 
 # def match_select(request, pk): 매치 선택
@@ -85,37 +77,38 @@ def match_register(request):
 # def match_open(request, pk):
 
 def match_update(request, pk):
-  
-  if request.method == "POST":
-    match_form = MatchRegisterForm(request.POST)
-    if match_form.is_valid():
-      id = match_form.save()
-      return redirect("/") # 수정된 페이지로 이동
-    
+
+    if request.method == "POST":
+        match_form = MatchRegisterForm(request.POST)
+        if match_form.is_valid():
+            id = match_form.save()
+            return redirect("/")  # 수정된 페이지로 이동
+
+        else:
+            return redirect("/")  # 다시 작성하기
+
     else:
-      return redirect("/") # 다시 작성하기
-  
-  else:
-    match_form = MatchRegisterForm()
-    context = {"match_form" : match_form}
-    return render(request, "html", context=context)
+        match_form = MatchRegisterForm()
+        context = {"match_form": match_form}
+        return render(request, "html", context=context)
 
 
-#매치 결정
-def match_resolve(request, pk): # pk = 매치 아이디
-  
-  if request.method == "POST":
-    match = get_object_or_404(MatchInfo, id = pk)
-    match.participant_id = request.user.pk
-    match.is_matched = True
-    match.save()
-    return redirect("/")
+# 매치 결정
+def match_resolve(request, pk):  # pk = 매치 아이디
 
-  return render(request, "html")
+    if request.method == "POST":
+        match = get_object_or_404(MatchInfo, id=pk)
+        match.participant_id = request.user.pk
+        match.is_matched = True
+        match.save()
+        return redirect("/")
+
+    return render(request, "html")
 
 
 def main(request, *args, **kwargs):
     alarm = Alarm.objects.filter(team_id=request.user.pk)
+    alarm = alarm.first()
     return render(request, "matchingMatch/main.html", {'alarm': alarm})
 
 
@@ -146,6 +139,7 @@ def check_endOfGame():
                     match_id=match
                 )
 
+
 def login_page(request):
     page = 'login'
 
@@ -153,18 +147,19 @@ def login_page(request):
         user = authenticate(
             username=request.POST['username'],
             password=request.POST['password']
-            )
+        )
 
         if user is not None:
             login(request, user)
             messages.info(request, '성공적으로 로그인 하셨습니다.')
-            return redirect('home') 
+            return redirect('home')
         else:
             messages.error(request, '이메일 혹은 비밀번호를 다시 확인해주세요.')
             return redirect('login')
-    
-    context = {'page':page}
+
+    context = {'page': page}
     return render(request, 'login_register.html', context)
+
 
 def register_page(request):
     form = CustomUserCreateForm()
@@ -180,18 +175,20 @@ def register_page(request):
         else:
             messages.error(request, '회원가입 도중에 문제가 발생하였습니다.')
 
-
     page = 'register'
-    context = {'page':page, 'form':form}
+    context = {'page': page, 'form': form}
     return render(request, 'login_register.html', context)
+
 
 def logout_user(request):
     logout(request)
     messages.info(request, '로그아웃 상태입니다.')
     return redirect('login')
 
+
 def home_page(request):
     return render(request, 'home.html')
+
 
 @login_required(login_url='/login')
 def account_page(request):
@@ -201,10 +198,10 @@ def account_page(request):
     # img = Image.open(user.avatar)
     # newsize = (10, 10)
     # img = img.resize(newsize)
-  
+
     # user.avatar = img
     # user.save()
     # user.save()
 
-    context = {'user':user}
+    context = {'user': user}
     return render(request, 'account.html', context)
